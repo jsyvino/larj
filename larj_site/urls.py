9ffdb60f7afc29189f96e74c4f8ae0d83af77e56
jsyvino <jsyvino@gmail.com>
2020-07-01 20:49:17 -0500

    bljfnesl'

--- larj_site/urls.py
+++ larj_site/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from emails.views import index
 
 urlpatterns = [
     path('emails/', include('emails.urls')),
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('/', index, name='index'),
 ]
